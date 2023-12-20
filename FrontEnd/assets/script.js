052b001e5db3d87d229495cfaa744da5a849971e
Pierre <ghueldre.pierre@laposte.net>
2023-12-20 17:47:18 +0100

    crée boutons filtres depuis API

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -5,6 +5,17 @@ function removeAllChildNodes(htmlElement) {
         htmlElement.removeChild(htmlElement.firstChild);
     }
 }
+
+function toKebabCase(text) {
+    text = text.toLowerCase();
+    text = text.replaceAll(/.'/g, ""); // enlève les >> l' d' etc.
+    text = text.replaceAll(/\p{Punctuation}/gu, ""); // enlève les isgnes de ponctuation
+    // if there is two or more consecutive spaces reduce the space to one space
+    // this is to avoid having more than one hyphen during the next replaceAll below this one
+    text = text.replaceAll(/\s{2,}/g, " ");
+    text = text.replaceAll(" ", "-");
+    return text;
+}
 /////////////////////////////
 
 function createGalleryItem(imageAddress, imageTitle) {
@@ -39,7 +50,56 @@ function createGalleryItemList(itemList) {
     domItemsContainer.append(temporaryItemsContainer);
 }
 
+////////////////// filtres
+function createFilterButtonList(data) {
+    const buttonContainerElement = document.body.querySelector("#portfolio .filters");
+    const templateElement = document.getElementById("template--filter-button");
+    const fragmentContainer = document.createDocumentFragment();
+    // ajoute le bouton "Tous" car il n'est pas dans les données
+    {
+        const allButton = createButton("Tous", "all", true);
+        fragmentContainer.append(allButton);
+    }
+    for(let i = 0; i !== data.length; ++ i) {
+        const name = data[i]["name"];
+        const newButton = createButton(name, toKebabCase(name));
+        fragmentContainer.append(newButton);
+    }
+
+    removeAllChildNodes(buttonContainerElement);
+    buttonContainerElement.append(fragmentContainer);
+    
+    function createButton(text, value, checked = false) {
+        const newButton = templateElement.content.cloneNode(true);
+        {
+            const radioInput = newButton.querySelector("input");
+            radioInput.setAttribute("id", value);
+            radioInput.setAttribute("value", value);
+            // par défaut
+            radioInput.setAttribute("name", "filter");
+
+            // new
+            if(checked) {
+                radioInput.checked = checked;
+            }
+        }
+        {
+            const label = newButton.querySelector("label");
+            label.setAttribute("for", value);
+            label.textContent = text;
+        }
+        return newButton;
+    }
+}
+
 ////////////////// main
+
+async function updateFilterButtonList(categoryListURL) {
+    const response = await fetch(categoryListURL);
+    // on reçoit les données sous la forme d'un tableau d'objets
+    const dataArray = await response.json();
+    createFilterButtonList(dataArray);
+}
 
 async function updateGallery(updateURL) {
     const response = await fetch(updateURL);
@@ -48,5 +108,6 @@ async function updateGallery(updateURL) {
     createGalleryItemList(dataArray);
 }
 
+updateFilterButtonList("http://localhost:5678/api/categories");
 // TODO : voir si l'on pourrait améliorer notre appel à l'API afin de ne recevoir que ce qui nous intéresse, à savoir l'URL de l'image et le titre...
 updateGallery("http://localhost:5678/api/works");
